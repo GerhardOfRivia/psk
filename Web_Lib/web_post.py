8d885bc1262d83df334f1b497c74a447a8c977ac
AsynchronousGillz <AsynchronousGillz@users.noreply.github.com>
2018-02-03 11:03:41 -0700

    Update web_post.py

--- Web_Lib/web_post.py
+++ Web_Lib/web_post.py
@@ -20,7 +20,7 @@ text = json.dumps(data, indexnt=4)
 
 data = urllib.parse.urlencode({'name': args.name, 'from': args.target , 'info': text})
 data = data.encode('utf-8')
-request = urllib.request.Request("http://antdash.colostate.edu/lib/make_data.php")
+request = urllib.request.Request("http://gillz.example.com/lib/make_data.php")
 
 # adding charset parameter to the Content-Type header.
 request.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
